217ecb9417fecadb344b4b12eb964fc84eae3f19
Prajwal Mavkar <mavkarprajwal@gmail.com>
2025-09-05 21:26:06 +0530

    Update DQL command.sql

--- DQL command.sql
+++ DQL command.sql
@@ -1,8 +1,24 @@
 use world;
+
 select * from city;
 select * from country;
 select * from countrylanguage;
 
+### where clause 
 
-#select *from table_name;
-#select col_name from table_name ;
+select * from city
+where city_pop >= 50000;
+
+
+### order by
+
+select * from city
+where city_pop >= 50000
+order by city_pop desc;
+
+### GROUP BY
+
+select * from city;
+select CountryCode,count(city_pop) 
+FROM city
+GROUP BY CountryCode;
